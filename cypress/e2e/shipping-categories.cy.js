b6538a771dbaddbffcf6f194db10ff3c0c4352fe
Leonardo Su <leonardo.su@ccc.ufcg.edu.br>
2023-06-05 23:35:20 -0300

    add filter tests

--- cypress/e2e/shipping-categories.cy.js
+++ cypress/e2e/shipping-categories.cy.js
@@ -17,6 +17,16 @@ const genCategory = (categoryName) => {
   cy.get('*[class^="ui labeled icon primary button"]').scrollIntoView().click();
 }
 
+const deleteCategory = (categoryName) => {
+  cy.clickInFirst('a[href="/admin/shipping-categories/"]');
+  cy.contains('tr', categoryName)
+  .within(() => {
+    cy.get('*[class^="ui red labeled icon button"]')
+      .click();
+  });
+  cy.get('*[class="ui green ok inverted button"]').click()
+}
+
 describe('shipping categories', () => {
   beforeEach(() => {
     cy.visit('/admin');
@@ -41,6 +51,7 @@ describe('shipping categories', () => {
     cy.get('*[class^="ui labeled icon primary button"]').scrollIntoView().click();
     // Assert that shipping category has been created.
     cy.get('body').should('contain', 'Shipping category has been successfully created.');
+    deleteCategory('33');
   });
   it('should delete a shipping category if the user click YES', () => {
     const categoryName = genString(15);
@@ -71,8 +82,9 @@ describe('shipping categories', () => {
 
     cy.get('body').should('not.contain', 'Shipping category has been successfully deleted.');
     cy.get('body').should('contain', categoryName);
+    deleteCategory(categoryName);
   });
-  it.only('edit a shipping category', () => {
+  it('edit a shipping category', () => {
     const categoryName = genString(15);
     genCategory(categoryName);
     cy.clickInFirst('a[href="/admin/shipping-categories/"]');
@@ -91,5 +103,97 @@ describe('shipping categories', () => {
     cy.get('body').should('contain', 'Shipping category has been successfully updated.');
     cy.get('body').should('contain', 'New Name 22');
     cy.get('body').should('contain', 'New Description 22');
+    deleteCategory(categoryName);
+  });
+  it('should filter equals category', () => {
+    cy.clickInFirst('a[href="/admin/shipping-categories/"]');
+
+    //Create some category
+    const categoryName1 = "RandomCategory";
+    const categoryName2 = "OtherName";
+    genCategory(categoryName1);
+    genCategory(categoryName2);
+
+    cy.clickInFirst('a[href="/admin/shipping-categories/"]');
+    cy.get('#criteria_search_type').select('equal');
+    cy.get('[id="criteria_search_value"]').type(categoryName1);
+
+    cy.get('[class="ui blue labeled icon button"').click();
+
+    
+    cy.contains('tr', categoryName1).should('exist');
+    cy.contains('tr', categoryName2).should('not.exist');
+
+    deleteCategory(categoryName1);
+    deleteCategory(categoryName2);
+  });
+  it('should filter Contains category', () => {
+    cy.clickInFirst('a[href="/admin/shipping-categories/"]');
+
+    //Create categories
+    const categoryName1 = "RandomCategory";
+    const categoryName2 = "RandCategory";
+    const categoryName3 = "OtherName";
+    genCategory(categoryName1);
+    genCategory(categoryName2);
+    genCategory(categoryName3);
+
+    // Set filter to 'contains'
+    cy.clickInFirst('a[href="/admin/shipping-categories/"]');
+    cy.get('#criteria_search_type').select('contains');
+    cy.get('[id="criteria_search_value"]').type('Rand');
+    cy.get('[class="ui blue labeled icon button"').click();
+
+    // Assert only category 1 and 2 appear
+    cy.contains('tr', categoryName1).should('exist');
+    cy.contains('tr', categoryName2).should('exist');
+    cy.contains('tr', categoryName3).should('not.exist');
+
+    deleteCategory(categoryName1);
+    deleteCategory(categoryName2);
+    deleteCategory(categoryName3);
+  });
+  it('should filter Not contains category', () => {
+    cy.clickInFirst('a[href="/admin/shipping-categories/"]');
+
+    //Create a category
+    const categoryName = "RandomCategory";
+    genCategory(categoryName);
+
+    // Set filter to 'no contains'
+    cy.clickInFirst('a[href="/admin/shipping-categories/"]');
+    cy.get('#criteria_search_type').select('not_contains');
+    cy.get('[id="criteria_search_value"]').type('Random');
+
+    cy.get('[class="ui blue labeled icon button"').click();
+
+    //Assert that category do not appear
+    cy.contains('tr', categoryName).should('not.exist');
+
+    deleteCategory(categoryName);
+  });
+  it('should filter Equals category', () => {
+    cy.clickInFirst('a[href="/admin/shipping-categories/"]');
+
+    //Create some category
+    const categoryName1 = "RandomCategory";
+    const categoryName2 = "OtherName";
+    genCategory(categoryName1);
+    genCategory(categoryName2);
+
+    // Set filter to 'equals'
+    cy.clickInFirst('a[href="/admin/shipping-categories/"]');
+    cy.get('#criteria_search_type').select('contains');
+    cy.get('[id="criteria_search_value"]').type(categoryName1);
+
+    cy.get('[class="ui blue labeled icon button"').click();
+
+    
+    //Assert that category 'Other name' do not appear
+    cy.contains('tr', categoryName1).should('exist');
+    cy.contains('tr', categoryName2).should('not.exist');
+
+    deleteCategory(categoryName1);
+    deleteCategory(categoryName2);
   });
 });
